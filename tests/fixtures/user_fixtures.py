32f89a17214af777500958014e79d467310da0e7
Gabriel Tavares <gsantostavares@gmail.com>
2023-06-30 13:19:35 -0300

    added test about check user input

--- tests/fixtures/user_fixtures.py
+++ tests/fixtures/user_fixtures.py
@@ -1,15 +1,27 @@
 import pytest
 from app.models.user import User
-from ...app.services.deal_with_csv import convert_list_of_lists_to_csv, convert_csv_to_dataframe
+from ...app.config.settings import session
+from ...app.services.deal_with_csv import convert_csv_to_dataframe, convert_list_of_lists_to_csv
 
-@pytest.fixture
-def insert_user_into_db_by_csv_file():
+@pytest.fixture()
+def insert_users_into_db_by_csv_file():
 
-    users_data = [['joao', 'joao@company.com', 'admin'],
-                  ['maria', 'maria@company.com', 'operacional'],
-                  ['ana', 'ana@company.com', 'operacional']
+    users_to_create_csv = [  ['name', 'email', 'user_type']
+                    ['joao', 'joao@company.com', 'admin'],
+                    ['maria', 'maria@company.com', 'operacional'],
+                    ['ana', 'ana@company.com', 'operacional']
                   ]
 
-    convert_list_of_lists_to_csv(data_list=users_data, filename='users', path_to_save='..\..\data_to_input')
-    convert_csv_to_dataframe(path_and_file_name='C:\Users\s1203595\OneDrive - Syngenta\Documents\aaplicacoes_e_codigos\smartsheet\data_to_input')
-        
+    convert_list_of_lists_to_csv(data_list=users_to_create_csv, filename='users', path_to_save='..\..\data_to_input')
+    users_from_dataframe = convert_csv_to_dataframe(path_and_file_name='C:\\Users\\s1203595\OneDrive - Syngenta\Documents\\aplicacoes_e_codigos\\smartsheet\\app\\teste.csv')
+
+    users_list = []
+    for i in users_from_dataframe.iterrows():
+        users_list.append(User(name=i[1][0], email=i[1][1], user_type=i[1][2]))
+    
+    session.add_all(users_list)
+
+    yield users_list
+
+    session.flush()
+    session.close()
