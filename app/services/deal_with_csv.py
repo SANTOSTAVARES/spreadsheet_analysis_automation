dd859c32220351183ae8a198e2c571b12026389e
Gabriel Tavares <gsantostavares@gmail.com>
2023-07-19 17:03:44 -0300

    deleted function convert csv to Dataframe

--- app/services/deal_with_csv.py
+++ app/services/deal_with_csv.py
@@ -1,13 +1,22 @@
 import csv
+import os
 from pandas import DataFrame, read_csv
 
-def convert_list_of_lists_to_csv(data_list: list, filename: str, path_to_save: str) -> None:
-    with open(f'{path_to_save}\{filename}.csv', 'w', newline='', encoding='UTF8') as csvfile:
+
+def convert_list_of_lists_to_text_file(data_list: list, file_name: str, path_to_save: str) -> None:
+    """The csv must have header."""
+    with open(f'{path_to_save}\{file_name}', 'w', newline='', encoding='UTF8') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows(data_list)
-    
+
     return None
 
-def convert_csv_to_dataframe(path_and_file_name: str) -> DataFrame:
-    
-    return read_csv(filepath_or_buffer=path_and_file_name, header=0)
+
+def delete_file_from_directory(file_name: str, path: str) -> None:
+
+    if os.path.isfile(f'{path}{file_name}'):
+        os.remove(f'{path}{file_name}')
+    else:
+        raise FileNotFoundError('File not found')
+
+    return None
