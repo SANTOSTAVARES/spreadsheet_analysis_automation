6d98532571c12a18174b428db0d48cca583e7a33
Gabriel Tavares <gsantostavares@gmail.com>
2023-07-21 16:22:41 -0300

    query get_all_users and get_user_by_email

--- app/core/users_repository.py
+++ app/core/users_repository.py
@@ -1,2 +1,15 @@
 from app.models.user import User
 from app.config.database import session
+from sqlalchemy import select
+
+
+def get_all_users() -> list[User]:
+    with session as s:
+        stmt = s.execute(select(User))
+        return stmt.scalars().all()
+
+
+def get_user_by_email(email: str):
+    with session as s:
+        stmt = s.execute(select(User).where(User.email == email))
+        return stmt.scalar()
