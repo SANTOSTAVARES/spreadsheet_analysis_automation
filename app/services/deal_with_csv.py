74de4b0bf0fb18e439f583df2f1602d8556e07ae
Gabriel Tavares <gsantostavares@gmail.com>
2023-08-01 10:50:37 -0300

    created CsvData class

--- app/services/deal_with_csv.py
+++ app/services/deal_with_csv.py
@@ -1,6 +1,8 @@
 import csv
 import os
 from pandas import DataFrame, read_csv
+from dataclasses import dataclass
+from app.models.user import User
 
 
 def convert_list_of_lists_to_text_file(data_list: list, file_name: str, path_to_save: str) -> None:
@@ -20,3 +22,21 @@ def delete_file_from_directory(file_name: str, path: str) -> None:
         raise FileNotFoundError('File not found')
 
     return None
+
+
+@dataclass
+class CsvData:
+
+    file_name: str
+    csv_file_path: str
+
+    def convert_csv_to_users_list(self) -> list[User]:
+
+        df = read_csv(f'{self.csv_file_path}{self.file_name}')
+
+        users_list = []
+        for i in df.iterrows():
+            users_list.append(
+                User(name=i[1][0], email=i[1][1], user_type=i[1][2]))
+
+        return users_list
