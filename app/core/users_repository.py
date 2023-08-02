9064728bf71f74de9d48b262275674a71d1184fe
Gabriel Tavares <gsantostavares@gmail.com>
2023-08-02 15:58:25 -0300

    added typing to query get_user_by_email

--- app/core/users_repository.py
+++ app/core/users_repository.py
@@ -9,7 +9,7 @@ def get_all_users() -> list[User]:
         return stmt.scalars().all()
 
 
-def get_user_by_email(email: str):
+def get_user_by_email(email: str) -> User:
     with session as s:
         stmt = s.execute(select(User).where(User.email == email))
         return stmt.scalar()
