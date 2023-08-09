e169b4edea45d04d2ec2b2e705d976b649deb473
Gabriel Tavares <gsantostavares@gmail.com>
2023-08-09 14:02:14 -0300

    smartsheet file update

--- app/sheets/smartsheet.py
+++ app/sheets/smartsheet.py
@@ -1,3 +1,4 @@
+from settings import setting
 from smartsheet_dataframe import get_sheet_as_df
 from pandas import to_datetime, to_numeric
 from pandas.core.frame import DataFrame
@@ -5,13 +6,13 @@ import requests
 import sys
 
 sys.path.append('..\\config')
-from settings import setting
 token = setting.SMARTSHEET_TOKEN
+
 
 class Smartsheet:
     def __init__(self, user_token: str = token) -> None:
         self.user_token = user_token
-        
+
     def get_bearer_authentication(self) -> dict:
         return {'Authorization': f'Bearer {self.user_token}'}
 
@@ -27,9 +28,9 @@ class Smartsheet:
             return e
         else:
             return response.json()["data"][0]
-        
+
     def get_sheet_as_dataframe(self, sheet_id: str) -> DataFrame:
-        
+
         try:
             df = get_sheet_as_df(token=self.user_token,
                                  sheet_id=sheet_id)
@@ -38,10 +39,11 @@ class Smartsheet:
         else:
             return df
 
+
 class DataTable:
     def __init__(self, data_table: DataFrame) -> None:
         self.data_table = data_table
-    
+
     def check_if_there_is_only_date_in_column(self, column_name: str) -> bool:
         try:
             to_datetime(self.data_table[column_name])
@@ -49,7 +51,7 @@ class DataTable:
             return False
         else:
             return True
-    
+
     def check_if_there_is_only_integer_in_column(self, column_name: str) -> bool:
         try:
             to_numeric(self.data_table[column_name])
