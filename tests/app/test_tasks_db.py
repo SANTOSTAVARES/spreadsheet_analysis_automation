aff33c13721bbf518ff83e5e823755ff2f01a191
Gabriel Tavares <gsantostavares@gmail.com>
2023-08-16 11:33:57 -0300

    test_get_tasks_with_true_status

--- tests/app/test_tasks_db.py
+++ tests/app/test_tasks_db.py
@@ -1,7 +1,7 @@
-from ..fixtures.tasks_fixtures import insert_tasks_with_both_status_into_db, insert_task_weekdays_into_db
+from ..fixtures.tasks_fixtures import insert_tasks_with_both_status_into_db
 from ..fixtures.sheets_fixtures import insert_into_users_sheets_table, insert_sheet_into_db
 from ..fixtures.user_fixtures import insert_users_into_db, create_csv_users_file
-from app.core.tasks_repository import get_task_by_task_id, get_first_task_in_db, get_true_taskweekdays_by_day, get_tasks_with_true_status_in_db
+from app.core.tasks_repository import get_first_task_in_db, get_tasks_with_true_status_in_db
 
 
 def test_get_task_by_task_id(insert_tasks_with_both_status_into_db):
