eaa91f4b8540430935d0bbed92659cba4193c14f
Gabriel Tavares <gsantostavares@gmail.com>
2023-08-29 16:58:54 -0300

    deleted table user_sheet

--- app/models/sheets.py
+++ app/models/sheets.py
@@ -3,21 +3,10 @@ import sqlalchemy as sa
 from ..config.database import Base
 
 
+@dataclass
 class Sheet(Base):
     __tablename__ = 'sheets'
 
     sheet_id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     identification_name: str = sa.Column(
         sa.String, nullable=False, index=True, unique=True)
-
-
-@dataclass
-class UserSheet(Base):
-    __tablename__ = 'users_sheets'
-
-    users_sheets_id: int = sa.Column(
-        sa.Integer, primary_key=True, autoincrement=True)
-    user_id: int = sa.Column(sa.ForeignKey(
-        "users.user_id", ondelete="CASCADE"), index=True, nullable=False)
-    sheet_id: int = sa.Column(sa.ForeignKey(
-        "sheets.sheet_id", ondelete="CASCADE"), index=True, nullable=False)
