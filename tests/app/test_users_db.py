84840466a7e8d5e863194c681227996707903618
Gabriel Tavares <gsantostavares@gmail.com>
2023-08-30 16:27:57 -0300

    insert_users_into_db update yield to get_all_users

--- tests/app/test_users_db.py
+++ tests/app/test_users_db.py
@@ -3,8 +3,7 @@ from app.queries.users_repository import get_all_users, get_user_by_email
 
 
 def test_get_user_by_email(insert_users_into_db):
-    df = insert_users_into_db
-    user_email = df['email'].loc[df.index[0]]
+    user_email = insert_users_into_db[0].email
     first_user_in_db = get_user_by_email(email=user_email)
 
     # Check if there is available in db.
@@ -13,20 +12,20 @@ def test_get_user_by_email(insert_users_into_db):
 
 def test_get_all_users(insert_users_into_db):
 
-    df = insert_users_into_db
+    users_inserted_in_db = insert_users_into_db
     users_in_db = get_all_users()
 
     # Check if there are users names available in databse
-    assert users_in_db[0].name == df['name'].loc[df.index[0]]
-    assert users_in_db[1].name == df['name'].loc[df.index[1]]
-    assert users_in_db[2].name == df['name'].loc[df.index[2]]
+    assert users_in_db[0].name == users_inserted_in_db[0].name
+    assert users_in_db[1].name == users_inserted_in_db[1].name
+    assert users_in_db[2].name == users_inserted_in_db[2].name
 
     # Check if there are users e-mails available in databse
-    assert users_in_db[0].email == df['email'].loc[df.index[0]]
-    assert users_in_db[1].email == df['email'].loc[df.index[1]]
-    assert users_in_db[2].email == df['email'].loc[df.index[2]]
+    assert users_in_db[0].email == users_inserted_in_db[0].email
+    assert users_in_db[1].email == users_inserted_in_db[1].email
+    assert users_in_db[2].email == users_inserted_in_db[2].email
 
     # Check if there are users type available in databse
-    assert users_in_db[0].user_type == df['user_type'].loc[df.index[0]]
-    assert users_in_db[1].user_type == df['user_type'].loc[df.index[1]]
-    assert users_in_db[2].user_type == df['user_type'].loc[df.index[2]]
+    assert users_in_db[0].user_type == users_inserted_in_db[0].user_type
+    assert users_in_db[1].user_type == users_inserted_in_db[1].user_type
+    assert users_in_db[2].user_type == users_inserted_in_db[2].user_type
