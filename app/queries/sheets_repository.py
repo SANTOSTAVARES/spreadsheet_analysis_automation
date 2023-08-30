bc16ad79bf010e5d7225b2f2a18ce30ae098ac9d
Gabriel Tavares <gsantostavares@gmail.com>
2023-08-30 14:01:36 -0300

    changed the folder name from models to queries

---
+++ app/queries/sheets_repository.py
@@ -0,0 +1,10 @@
+from app.models.sheets import Sheet
+from app.config.database import session
+from sqlalchemy import select
+
+
+def get_sheet_by_identification_name(identification_name: str) -> Sheet:
+    with session as s:
+        stmt = s.execute(select(Sheet).where(
+            Sheet.identification_name == identification_name))
+        return stmt.scalar()
---
+++ app/queries/tasks_repository.py
@@ -0,0 +1,42 @@
+from app.services.routine import attribute_from_taksweekday_about_current_day
+from app.models.tasks import Task, TaskWeekday, AchievedTask
+from app.config.database import session
+from sqlalchemy import select
+
+
+def get_task_by_task_id(task_id: int) -> Task:
+    with session as s:
+        stmt = s.execute(select(Task).where(Task.task_id == task_id))
+        return stmt.scalars()
+
+
+def get_first_task_in_db() -> Task:
+    with session as s:
+        stmt = s.execute(select(Task))
+        return stmt.scalar()
+
+
+def get_tasks_with_true_status_in_db():
+    with session as s:
+        stmt = s.execute(select(Task).where(Task.task_status == True))
+        return stmt.fetchall()
+
+
+def get_taskweekday_by_current_day():
+    current_day = attribute_from_taksweekday_about_current_day()
+    with session as s:
+        stmt = s.execute(select(TaskWeekday).where(current_day == True))
+        return stmt.fetchall()
+
+
+def get_first_achieved_task_in_db():
+    with session as s:
+        stmt = s.execute(select(AchievedTask))
+        return stmt.scalar()
+
+
+def get_achieved_tasks_by_task_id(task_id: int):
+    with session as s:
+        stmt = s.execute(select(AchievedTask).where(
+            AchievedTask.task_id == task_id))
+        return stmt.fetchall()
