5a590ce4d7468053a3601ffce1e7725da3b3c56d
Gabriel Tavares <gsantostavares@gmail.com>
2023-09-08 17:06:58 -0300

    update on test_get_all_users comment

--- tests/app/test_users_db.py
+++ tests/app/test_users_db.py
@@ -15,17 +15,17 @@ def test_get_all_users(insert_users_into_db):
     users_inserted_in_db = insert_users_into_db
     users_in_db = get_all_users()
 
-    # Check if there are users names available in databse
+    # Check if there are users names available in databse.
     assert users_in_db[0].name == users_inserted_in_db[0].name
     assert users_in_db[1].name == users_inserted_in_db[1].name
     assert users_in_db[2].name == users_inserted_in_db[2].name
 
-    # Check if there are users e-mails available in databse
+    # Check if there are users e-mails available in databse.
     assert users_in_db[0].email == users_inserted_in_db[0].email
     assert users_in_db[1].email == users_inserted_in_db[1].email
     assert users_in_db[2].email == users_inserted_in_db[2].email
 
-    # Check if there are users type available in databse
+    # Check if there are users type available in databse.
     assert users_in_db[0].user_type == users_inserted_in_db[0].user_type
     assert users_in_db[1].user_type == users_inserted_in_db[1].user_type
     assert users_in_db[2].user_type == users_inserted_in_db[2].user_type
