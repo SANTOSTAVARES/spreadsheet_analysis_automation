4514a0444853b752a6e01c804f54a443f893a922
Gabriel Tavares <gsantostavares@gmail.com>
2023-09-11 17:01:46 -0300

    Must have to add statement about achieved tasks

--- app/queries/routine_repository.py
+++ app/queries/routine_repository.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from app.models.sheets import Sheet
 from app.models.user import User
-from app.models.tasks import Task, TaskRuntime, UserTask
+from app.models.tasks import Task, TaskRuntime, UserTask, TaskWeekday
 from app.config.database import session
 from sqlalchemy import select
+from app.services.routine import attribute_from_taksweekday_about_current_day
 
 
 def get_sheet_by_identification_name(identification_name: str) -> Sheet:
@@ -12,11 +14,28 @@ def get_sheet_by_identification_name(identification_name: str) -> Sheet:
         return stmt.scalar()
 
 
-def teste_query():
+def get_all_tasks_have_to_be_done_now():
+    time_now = datetime.now().time()
+    current_day = attribute_from_taksweekday_about_current_day()
+
     with session as s:
-        stmt = s.execute(select(Task.main_column, TaskRuntime.runtime, Task.task_id)
+        stmt = s.execute(select(TaskRuntime.runtime,
+                                Task.task_type,
+                                Sheet.identification_name,
+                                Task.main_column,
+                                Task.auxiliary_column,
+                                Task.reference_values,
+                                User.email,
+                                TaskRuntime.runtime, Sheet.sheet_id, Task.task_id, TaskWeekday.task_weekday_id, TaskRuntime.tasks_runtime_id)
+                         .join(TaskWeekday, TaskWeekday.task_id == Task.task_id)
                          .join(TaskRuntime, TaskRuntime.task_id == Task.task_id)
                          .join(Sheet, Sheet.sheet_id == Task.sheet_id)
                          .join(UserTask, UserTask.task_id == Task.task_id)
-                         .where(Task.task_status == True))
+                         .where(
+            (Task.task_status == True)
+            &
+            (current_day == True)
+            &
+            (TaskRuntime.runtime < time_now)
+        ))
         return stmt.fetchall()
--- tests/app/test_routine.py
+++ tests/app/test_routine.py
@@ -1,9 +1,9 @@
-from app.queries.routine_repository import teste_query
-from tests.fixtures.tasks_fixtures import insert_tasks_runtime_into_db, insert_tasks_with_both_status_into_db, insert_users_tasks_into_db
+from app.queries.routine_repository import get_all_tasks_have_to_be_done_now
+from tests.fixtures.tasks_fixtures import insert_tasks_runtime_into_db, insert_tasks_with_both_status_into_db, insert_users_tasks_into_db, insert_task_weekdays_into_db
 from tests.fixtures.sheets_fixtures import insert_sheet_into_db
 from tests.fixtures.user_fixtures import insert_users_into_db, create_csv_users_file
 
 
-def test_t(insert_tasks_runtime_into_db, insert_users_tasks_into_db):
-    t = teste_query()
+def test_routine______(insert_tasks_runtime_into_db, insert_users_tasks_into_db, insert_task_weekdays_into_db):
+    t = get_all_tasks_have_to_be_done_now()
     breakpoint()
