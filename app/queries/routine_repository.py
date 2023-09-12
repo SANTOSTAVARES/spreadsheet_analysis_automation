6d7aaa0737cc796ec950ab778a9fe34f3c18a33e
Gabriel Tavares <gsantostavares@gmail.com>
2023-09-12 11:16:47 -0300

    query get_all_tasks_have_to_be_done_now

--- app/queries/routine_repository.py
+++ app/queries/routine_repository.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from app.models.sheets import Sheet
-from app.models.user import User
-from app.models.tasks import Task, TaskRuntime, UserTask, TaskWeekday
+from app.models.tasks import Task, TaskRuntime, TaskWeekday, AchievedTask
 from app.config.database import session
 from sqlalchemy import select
 from app.services.routine import attribute_from_taksweekday_about_current_day
@@ -16,7 +15,7 @@ def get_sheet_by_identification_name(identification_name: str) -> Sheet:
 
 def get_all_tasks_have_to_be_done_now():
     time_now = datetime.now().time()
-    current_day = attribute_from_taksweekday_about_current_day()
+    current_day_on_taskweekday_object = attribute_from_taksweekday_about_current_day()
 
     with session as s:
         stmt = s.execute(select(TaskRuntime.runtime,
@@ -25,16 +24,14 @@ def get_all_tasks_have_to_be_done_now():
                                 Task.main_column,
                                 Task.auxiliary_column,
                                 Task.reference_values,
-                                User.email,
-                                TaskRuntime.runtime, Sheet.sheet_id, Task.task_id, TaskWeekday.task_weekday_id, TaskRuntime.tasks_runtime_id)
+                                TaskRuntime.tasks_runtime_id)
                          .join(TaskWeekday, TaskWeekday.task_id == Task.task_id)
                          .join(TaskRuntime, TaskRuntime.task_id == Task.task_id)
                          .join(Sheet, Sheet.sheet_id == Task.sheet_id)
-                         .join(UserTask, UserTask.task_id == Task.task_id)
                          .where(
             (Task.task_status == True)
             &
-            (current_day == True)
+            (current_day_on_taskweekday_object == True)
             &
             (TaskRuntime.runtime < time_now)
         ))
