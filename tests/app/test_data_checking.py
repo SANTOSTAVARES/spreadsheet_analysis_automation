236aa8d31f756437e10b02de918da3164b012d09
Gabriel Tavares <gsantostavares@gmail.com>
2023-09-14 14:58:24 -0300

    test_check_if_there_is_only_integer_in_column

--- tests/app/test_data_checking.py
+++ tests/app/test_data_checking.py
@@ -11,6 +11,20 @@ def test_if_there_is_only_date_in_column(create_dataframe):
     # Check if there is only date on 'only_date' column.
     assert data_to_check.do_check() == True
 
-    # Check if the function return False, when there is not date on column.
+    # Check if the function return False, when there is not date on 'only_string' column.
+    data_to_check.main_column = 'only_string'
+    assert data_to_check.do_check() == False
+
+
+def test_check_if_there_is_only_integer_in_column(create_dataframe):
+    df = create_dataframe
+    data_to_check = DataChecking(data_table=df,
+                                 checking_type='02',
+                                 main_column='only_integer')
+
+    # Check if there is only date on 'only_integer' column.
+    assert data_to_check.do_check() == True
+
+    # Check if the function return False, when there is not integer on 'only_string' column.
     data_to_check.main_column = 'only_string'
     assert data_to_check.do_check() == False
