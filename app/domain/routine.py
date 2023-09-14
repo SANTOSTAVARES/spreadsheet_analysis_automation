8d6c2883f681509bce4e674e12074f584a220c73
Gabriel Tavares <gsantostavares@gmail.com>
2023-09-14 14:59:38 -0300

    routine module creation

--- app/domain/routine.py
+++ app/domain/routine.py
@@ -1,9 +1,15 @@
 from app.queries.routine_repository import get_all_tasks_have_to_be_done_now
-from app.services.sheets import Smartsheet
+from app.services.sheets import Smartsheet, DataChecking
 
 
 def do_tasks():
 
     tasks = get_all_tasks_have_to_be_done_now()
     for t in tasks:
-        df = Smartsheet(sheet_id=t[0].sheet_name).get_sheet_as_dataframe()
+
+        task = DataChecking(data_table=Smartsheet(sheet_id=t[0].sheet_name).get_sheet_as_dataframe(),
+                            checking_type=t[0].task_type,
+                            main_column=t[0].main_column,
+                            auxiliary_column=t[0].auxiliary_column,
+                            reference_values=t[0].reference_values
+                            ).do_check()
