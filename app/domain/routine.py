572b0e02f8939d9082a2867e5d2470c8b84a805f
Gabriel Tavares <gsantostavares@gmail.com>
2023-10-04 14:20:48 -0300

    DataChecking return row and value outside rule

--- app/domain/routine.py
+++ app/domain/routine.py
@@ -1,15 +1,26 @@
+from datetime import date
 from app.queries.routine_repository import get_all_tasks_have_to_be_done_now
 from app.services.sheets import Smartsheet, DataChecking
+from app.queries.tasks_repository import get_achieved_task_by_tasks_runtime_id_and_created_at
 
 
 def do_tasks():
 
-    tasks = get_all_tasks_have_to_be_done_now()
-    for t in tasks:
+    tasks_to_be_done = get_all_tasks_have_to_be_done_now()
+    for task in tasks_to_be_done:
+        achieved_task = get_achieved_task_by_tasks_runtime_id_and_created_at(
+            tasks_runtime_id=task["tasks_runtime_id"], created_at=date.today())
 
-        task = DataChecking(data_table=Smartsheet(sheet_id=t[0].sheet_name).get_sheet_as_dataframe(),
-                            checking_type=t[0].task_type,
-                            main_column=t[0].main_column,
-                            auxiliary_column=t[0].auxiliary_column,
-                            reference_values=t[0].reference_values
-                            ).do_check()
+        if len(achieved_task) == 0:
+            df = Smartsheet(
+                sheet_id=task["sheet_name"]).get_sheet_as_dataframe()
+
+            row_value_outside_rule = DataChecking(data_table=df,
+                                                  checking_type=task["task_type"],
+                                                  main_column=task["main_column"],
+                                                  auxiliary_column=task["auxiliary_column"],
+                                                  reference_values=task["reference_values"]
+                                                  ).do_check()
+
+        else:
+            achieved_task.pop(0)
