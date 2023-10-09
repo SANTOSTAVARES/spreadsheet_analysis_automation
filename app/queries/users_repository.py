b7c23db25c3e3427190cc2afd76c45809c0f040a
Gabriel Tavares <gsantostavares@gmail.com>
2023-10-09 16:40:51 -0300

    query get_users_by_task_id

--- app/queries/users_repository.py
+++ app/queries/users_repository.py
@@ -1,6 +1,7 @@
-from app.models.user import User
-from app.config.database import session
 from sqlalchemy import select
+from app.config.database import session
+from app.models.user import User
+from app.models.tasks import UserTask
 
 
 def get_all_users() -> list[User]:
@@ -13,3 +14,10 @@ def get_user_by_email(email: str) -> User:
     with session as s:
         stmt = s.execute(select(User).where(User.email == email))
         return stmt.scalar()
+
+
+def get_users_by_task_id(task_id: int):
+    with session as s:
+        stmt = s.execute(select(User).join(UserTask, UserTask.user_id ==
+                                           User.user_id).where(UserTask.task_id == task_id)).all()
+    return stmt
