f27e0e70895b9f55bd14a364d8de8ca7a6b4a3b8
Gabriel Tavares <gsantostavares@gmail.com>
2023-11-20 17:10:05 -0300

    improving do_task function

--- app/domain/routine.py
+++ app/domain/routine.py
@@ -1,7 +1,8 @@
 from datetime import date
+from app.queries.users_repository import get_users_by_task_id
 from app.queries.routine_repository import get_all_tasks_have_to_be_done_now
-from app.services.sheets import Smartsheet, DataChecking
 from app.queries.tasks_repository import get_achieved_task_by_tasks_runtime_id_and_created_at
+from app.services.sheets import Smartsheet, DataChecking
 from app.services.email import send_email
 from app.services.html import list_of_row_value_outside_rule_converted_to_html_table_lines
 
@@ -11,7 +12,8 @@ def do_tasks():
     tasks_to_be_done = get_all_tasks_have_to_be_done_now()
     for task in tasks_to_be_done:
         achieved_task = get_achieved_task_by_tasks_runtime_id_and_created_at(
-            tasks_runtime_id=task["tasks_runtime_id"], created_at=date.today())
+            tasks_runtime_id=task["tasks_runtime_id"],
+            created_at=date.today())
 
         if len(achieved_task) == 0:
 
@@ -25,22 +27,42 @@ def do_tasks():
                                          auxiliary_column=task["auxiliary_column"],
                                          reference_values=task["reference_values"]
                                          )
+            del df
 
-            list_of_row_value_outside_rule_and_rule_description = data_checking.do_check()
+            list_of_row_value_outside_rule_and_rule_description = data_checking.do_checking_and_send_email()
+            rule_description = list_of_row_value_outside_rule_and_rule_description[1]
+            row_value_outside_rule = list_of_row_value_outside_rule_and_rule_description[0]
 
             row_value_outside_rule_as_html = list_of_row_value_outside_rule_converted_to_html_table_lines(
-                row_value_outside_rule=list_of_row_value_outside_rule_and_rule_description[0])
+                row_value_outside_rule=row_value_outside_rule)
 
-            # sheet_information = smartsheet.get_sheet_information_by_sheet_id()
-            sheet_information = {"Permalink": "", "name": ""}
+            sheet_information = smartsheet.get_sheet_information_by_sheet_id()
+
+            users = get_users_by_task_id(task_id=task["task_id"])
+
+            emails_list = [user[0].email for user in users]
 
             if len(list_of_row_value_outside_rule_and_rule_description[0]) > 0:
                 send_email(sheet_identification=task["sheet_name"],
                            sheet_information=sheet_information,
-                           rule_description=list_of_row_value_outside_rule_and_rule_description[1],
+                           rule_description=rule_description,
                            main_column=task["main_column"],
                            row_value_outside_rule=row_value_outside_rule_as_html,
-                           recipients_emails="")
-            del df
+                           recipients_emails=emails_list)
+
+        else:
+            achieved_task.pop(0)
+
+
+def _do_tasks():
+
+    tasks_to_be_done = get_all_tasks_have_to_be_done_now()
+    for task in tasks_to_be_done:
+        achieved_task = get_achieved_task_by_tasks_runtime_id_and_created_at(
+            tasks_runtime_id=task["tasks_runtime_id"],
+            created_at=date.today())
+
+        if len(achieved_task) == 0:
+            data_checking = DataChecking()
         else:
             achieved_task.pop(0)
