7ed9e240d07f1ed6d11bcdd3620c7dd969b4990e
Gabriel Tavares <gsantostavares@gmail.com>
2023-11-20 17:10:58 -0300

    changing dependent function name

--- tests/app/test_data_checking.py
+++ tests/app/test_data_checking.py
@@ -9,22 +9,8 @@ def test_if_there_is_only_date_in_column(create_dataframe):
                                  main_column='only_date')
 
     # Check if there is only date on 'only_date' column.
-    assert data_to_check.do_check() == True
+    assert data_to_check.do_checking_and_send_email() == True
 
     # Check if the function return False, when there is not date on 'only_string' column.
     data_to_check.main_column = 'only_string'
-    assert data_to_check.do_check() == False
-
-
-def test_check_if_there_is_only_integer_in_column(create_dataframe):
-    df = create_dataframe
-    data_to_check = DataChecking(data_table=df,
-                                 checking_type='02',
-                                 main_column='only_integer')
-
-    # Check if there is only date on 'only_integer' column.
-    assert data_to_check.do_check() == True
-
-    # Check if the function return False, when there is not integer on 'only_string' column.
-    data_to_check.main_column = 'only_string'
-    assert data_to_check.do_check() == False
+    assert data_to_check.do_checking_and_send_email() == False
