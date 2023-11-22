caf250bd1197c0160acc48aa7472defb5d5a6e4c
Gabriel Tavares <gsantostavares@gmail.com>
2023-11-22 15:49:29 -0300

    function insert_achieved_task_into_db

---
+++ app/domain/sql/dml/insert.py
@@ -0,0 +1,13 @@
+from app.models.tasks import AchievedTask
+from app.config.database import session
+
+
+def insert_achieved_task_into_db(tasks_runtime_id: int) -> AchievedTask:
+    achieved_task = AchievedTask(
+        tasks_runtime_id=tasks_runtime_id)
+
+    session.add(achieved_task)
+    session.commit()
+    session.close()
+
+    return achieved_task
