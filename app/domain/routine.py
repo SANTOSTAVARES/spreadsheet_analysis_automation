57d2b7b66689020a8a12022e54e7f438ee155969
Gabriel Tavares <gsantostavares@gmail.com>
2023-11-22 13:19:02 -0300

    create analyze_sheet_and_record_cheking_into_db

--- app/domain/routine.py
+++ app/domain/routine.py
@@ -1,4 +1,6 @@
 from datetime import date
+from app.config.database import session
+from app.models.tasks import AchievedTask
 from app.queries.users_repository import get_users_by_task_id
 from app.queries.routine_repository import get_all_tasks_have_to_be_done_now
 from app.queries.tasks_repository import get_achieved_task_by_tasks_runtime_id_and_created_at
@@ -7,54 +9,7 @@ from app.services.email import send_email
 from app.services.html import list_of_row_value_outside_rule_converted_to_html_table_lines
 
 
-def do_tasks():
-
-    tasks_to_be_done = get_all_tasks_have_to_be_done_now()
-    for task in tasks_to_be_done:
-        achieved_task = get_achieved_task_by_tasks_runtime_id_and_created_at(
-            tasks_runtime_id=task["tasks_runtime_id"],
-            created_at=date.today())
-
-        if len(achieved_task) == 0:
-
-            smartsheet = Smartsheet(
-                sheet_id=task["sheet_name"])
-            df = smartsheet.get_sheet_as_dataframe()
-
-            data_checking = DataChecking(data_table=df,
-                                         checking_type=task["task_type"],
-                                         main_column=task["main_column"],
-                                         auxiliary_column=task["auxiliary_column"],
-                                         reference_values=task["reference_values"]
-                                         )
-            del df
-
-            list_of_row_value_outside_rule_and_rule_description = data_checking.do_checking_and_send_email()
-            rule_description = list_of_row_value_outside_rule_and_rule_description[1]
-            row_value_outside_rule = list_of_row_value_outside_rule_and_rule_description[0]
-
-            row_value_outside_rule_as_html = list_of_row_value_outside_rule_converted_to_html_table_lines(
-                row_value_outside_rule=row_value_outside_rule)
-
-            sheet_information = smartsheet.get_sheet_information_by_sheet_id()
-
-            users = get_users_by_task_id(task_id=task["task_id"])
-
-            emails_list = [user[0].email for user in users]
-
-            if len(list_of_row_value_outside_rule_and_rule_description[0]) > 0:
-                send_email(sheet_identification=task["sheet_name"],
-                           sheet_information=sheet_information,
-                           rule_description=rule_description,
-                           main_column=task["main_column"],
-                           row_value_outside_rule=row_value_outside_rule_as_html,
-                           recipients_emails=emails_list)
-
-        else:
-            achieved_task.pop(0)
-
-
-def _do_tasks():
+def analyze_sheet_and_record_cheking_into_db():
 
     tasks_to_be_done = get_all_tasks_have_to_be_done_now()
     for task in tasks_to_be_done:
@@ -70,5 +25,11 @@ def _do_tasks():
                          main_column=task["main_column"],
                          auxiliary_column=task["auxiliary_column"],
                          reference_values=task["reference_values"]).do_checking_and_send_email()
+
+            achieved_task = AchievedTask(
+                tasks_runtime_id=task["tasks_runtime_id"])
+            session.add(achieved_task)
+            session.commit()
+            return task
         else:
             achieved_task.pop(0)
