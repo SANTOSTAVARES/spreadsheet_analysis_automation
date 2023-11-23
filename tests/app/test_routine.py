997e8224ef3d741b0836f73c1dc7913196a39983
Gabriel Tavares <gsantostavares@gmail.com>
2023-11-23 16:59:44 -0300

    upgrade test_routine

--- tests/app/test_routine.py
+++ tests/app/test_routine.py
@@ -1,7 +1,7 @@
-from app.domain.routine import do_tasks
+from app.domain.routine import analyze_sheet_and_record_cheking_into_db
 from tests.fixtures.real_user_and_sheet_fixtures import insert_real_user_and_sheet_into_db
 
 
 def test_routine(insert_real_user_and_sheet_into_db):
     real_user_and_sheet = insert_real_user_and_sheet_into_db
-    do_tasks()
+    analyze_sheet_and_record_cheking_into_db()
