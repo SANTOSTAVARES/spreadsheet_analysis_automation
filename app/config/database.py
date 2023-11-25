1e19fef3845da1a3a192f414be5134f8a2c456d6
Gabriel Tavares <gsantostavares@gmail.com>
2023-11-24 23:33:50 -0300

    import order correction

--- app/config/database.py
+++ app/config/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
-from app.config.settings import setting
 from sqlalchemy.orm import sessionmaker, declarative_base
-
+from app.config.settings import setting
 
 database_url = setting.DATABASE_URL
 engine = create_engine(database_url, echo=True)
